ba96d939c0a9c36a151f23c617eff9d91d138e75
Rory Budnick <rorybudnick@gmail.com>
2016-05-22 15:07:33 -0700

    annotate models, specify rspec

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: locations
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  street     :string
+#  street_2   :string
+#  city       :string
+#  state      :string
+#  country    :string
+#  notes      :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Location < ActiveRecord::Base
   has_and_belongs_to_many :students
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: students
+#
+#  id           :integer          not null, primary key
+#  first_name   :string
+#  last_name    :string
+#  grade        :string
+#  availability :string
+#  notes        :text
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Student < ActiveRecord::Base
   has_and_belongs_to_many :locations
 end
