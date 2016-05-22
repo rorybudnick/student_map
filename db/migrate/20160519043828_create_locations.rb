7a2272935a3c7cad07837a20df7c74d5fe5801cf
Rory Budnick <rorybudnick@gmail.com>
2016-05-22 14:23:25 -0700

    use postgres, add rspec and pry, run migrations

--- db/migrate/20160519043828_create_locations.rb
+++ db/migrate/20160519043828_create_locations.rb
@@ -2,8 +2,12 @@ class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
       t.string :name
-      t.string :address
-      t.string :notes
+      t.string :street
+      t.string :street_2
+      t.string :city
+      t.string :state
+      t.string :country
+      t.text :notes
       t.timestamps null: false
     end
   end
